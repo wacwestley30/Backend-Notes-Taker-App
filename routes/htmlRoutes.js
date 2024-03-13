9d78b527a22dfc0c27ddd1074ec877b07a076e5b
wacwestley30 <westleycervantes@gmail.com>
2024-03-13 16:28:16 -0400

    index.js reverted back to starter code. Problem resolved by placing /notes route before *. App working as intended both in preview on insomnia and live with npm start. Needs proper README and uploaded to heroku.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'))
-});
-
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
 });
 
 module.exports = router;
